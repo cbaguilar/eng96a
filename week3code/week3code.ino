3910caf2cbf21222e2408b6b8081fa64305f1dbe
Christian Aguilar <cbaguilar@ucla.edu>
2019-10-19 15:31:29 -0700

    Added function to read serial input.

--- week3code/week3code.ino
+++ week3code/week3code.ino
@@ -1,16 +1,31 @@
 //Team 2: Christian Aguilar, Daniel Roji, Jason Salgado
 //Defines our Wiring according to our hardware 
-#define ENA A0 
-#define IN1 13 
-#define IN2 12   
+
+//wiring for left motor
+#define ENA 5 
+#define IN1 6 
+#define IN2 7  
+
+ 
+//wiring for right motor
+#define ENB 3
+#define IN3 2
+#define IN4 4
+
+
 #define Trig 6
 #define Echo 5 
 
 void setup() {
-  // SetUp for Motor 
+  // SetUp for Motor Left
   pinMode(ENA,OUTPUT); 
   pinMode(IN1,OUTPUT);  
   pinMode(IN2,OUTPUT);
+
+  // SetUp for Motor Right
+  pinMode(ENB,OUTPUT); 
+  pinMode(IN3,OUTPUT);  
+  pinMode(IN4,OUTPUT);
 
   //SetUp for Ultra-Sonic Sensor  
   pinMode(Trig,OUTPUT); 
@@ -24,21 +39,60 @@ void setup() {
 }
 
 void loop() {
-  forward();  
+  /*
+  turn();  
   delay(1000); 
   stop1(); 
   delay(1000); 
+  */
+  String he = readLine();
+  Serial.print(he);
+  delay(100);
 } 
 
-void forward(int speed){ 
-  digitalWrite(ENA,speed); 
+String readLine(){
+  String command;
+  char nextByte = 0;
+  if (Serial.available() > 0) {
+    for (int i = 0; i < 100; i++) {
+      nextByte = Serial.read();
+      delay(100);
+      Serial.print(nextByte);
+      command+=nextByte;
+      if (nextByte == '\n') {break;}
+    }
+  }
+  return command;
+}
+
+void forward(){ 
+  digitalWrite(ENA,HIGH); 
   digitalWrite(IN1,HIGH); 
   digitalWrite(IN2,LOW); 
+
+  digitalWrite(ENB,HIGH); 
+  digitalWrite(IN3,HIGH); 
+  digitalWrite(IN4,LOW); 
     
   }
+
+void turn(){
+  digitalWrite(ENA,HIGH); 
+  digitalWrite(IN1,LOW); 
+  digitalWrite(IN2,HIGH); 
+
+  digitalWrite(ENB,HIGH); 
+  digitalWrite(IN3,HIGH); 
+  digitalWrite(IN4,LOW); 
+  
+}
 
 void stop1(){   
   digitalWrite(ENA,LOW); 
   digitalWrite(IN1,LOW); 
-  digitalWrite(IN2,LOW); 
+  digitalWrite(IN2,LOW);
+
+  digitalWrite(ENB,LOW); 
+  digitalWrite(IN3,LOW); 
+  digitalWrite(IN4,LOW); 
   }
