08035dd1b2d05c16e7f362b88c40629eae969488
Christian Aguilar <cbaguilar@ucla.edu>
2019-10-20 17:12:24 -0700

    Added experimental joystick control!

--- remote.py
+++ remote.py
@@ -1,9 +1,11 @@
 import serial
 import curses
 
+ser = serial.Serial('/dev/rfcomm10')
+
 
 screen = curses.initscr()
-curses.echo()
+curses.noecho()
 curses.cbreak()
 
 
@@ -11,7 +13,31 @@ print("Please specify a ch")
 a = screen.getch()
 print(a)
 
+
+CHAR = 0;
+lastchar = CHAR;
+while True:
+    CHAR = (screen.getch())
+    print(CHAR)
+    if lastchar != CHAR:
+        if CHAR == -1:
+            ser.write("stop\n")
+        elif CHAR == ord('w'):
+            ser.write('w\n')
+        elif CHAR == ord('s'):
+            ser.write('s\n')
+        elif CHAR == ord('p'):
+            ser.write('distance\n')
+        elif CHAR == ord('a'):
+            ser.write('a\n')
+        elif CHAR == ord('d'):
+            ser.write('d\n')
+        elif CHAR ==ord('q'):
+            ser.write('stop\n')
+            break
+    elif CHAR == -1:
+        pass    
+    lastchar = CHAR
+ser.close()
 curses.endwin()
 exit()
-#ser = serial.Serial(port)
-#print(ser.name)
